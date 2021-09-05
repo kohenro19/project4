5bf9386a1e93ca36c362cbbaf20947f54553740f
kohenro19 <kohenro19@gmail.com>
2021-09-05 19:32:26 +0900

    20210905r1

--- pos-system.py
+++ pos-system.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+ITEM_MASTER_CSV_PATH="./item_master.csv"
+RECEIPT_FOLDER="./receipt"
+
 ### 商品クラス
 class Item:
     def __init__(self,item_code,item_name,price):
@@ -26,14 +31,34 @@ class Order:
                 if order_item_code == m.item_code:
                     print("{}".format(m.item_name)+"の金額: "+"{}".format(m.price))
 
+def add_item_master_by_csv(csv_path):
+        print("------- マスタ登録開始 ---------")
+        item_master=[]
+        count=0
+        try:
+            item_master_df=pd.read_csv(csv_path,dtype={"item_code":object}) # CSVでは先頭の0が削除されるためこれを保持するための設定
+            for item_code,item_name,price in zip(list(item_master_df["item_code"]),list(item_master_df["item_name"]),list(item_master_df["price"])):
+                item_master.append(Item(item_code,item_name,price))
+                print("{}({})".format(item_name,item_code))
+                count+=1
+            print("{}品の登録を完了しました。".format(count))
+            print("------- マスタ登録完了 ---------")
+            return item_master
+        except:
+            print("マスタ登録が失敗しました")
+            print("------- マスタ登録完了 ---------")
+            sys.exit()
+            
 ### メイン処理
 def main():
     # マスタ登録
-    item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
-    
+    item_master=add_item_master_by_csv(ITEM_MASTER_CSV_PATH) # CSVからマスタへ登録
+    order=Order(item_master) 
+    # item_master=[]
+    # path = "./item_master.csv"
+    # df = pd.read_csv(path)
+    # item_master.append(Item(df["item_code"], df["item_name"], df["price"]))
+
     # オーダー登録
     order=Order(item_master)
     order.add_item_order()
